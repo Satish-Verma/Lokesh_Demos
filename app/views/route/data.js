db3a961c3f77317358396917f77d4ab6e3119b7b
Satish-Verma <sattyto@gmail.com>
2017-01-23 13:17:03 +0530

    Create data.js

---
+++ app/views/route/data.js
@@ -0,0 +1,92 @@
+[
+    {
+        "fname": "Lokesh",
+        "lname": "Sharma",
+        "tel": "99803-49924",
+        "address": "Noida",
+        "city": "G B Nagar",
+        "state": "UP",
+        "zip": "201304"
+    },
+    {
+        "fname": "Anuj",
+        "lname": "Gupta",
+        "tel": "99834-45465",
+        "address": "Shakarpur",
+        "city": "Delhi",
+        "state": "New Delhi",
+        "zip": "110011"
+    },
+    {
+        "fname": "Avinash",
+        "lname": "Sharma",
+        "tel": "99803-49924",
+        "address": "Noida",
+        "city": "G B Nagar",
+        "state": "UP",
+        "zip": "201304"
+    },
+    {
+        "fname": "Rajesh",
+        "lname": "Sharma",
+        "tel": "99803-49924",
+        "address": "Noida",
+        "city": "G B Nagar",
+        "state": "UP",
+        "zip": "201304"
+    },
+    {
+        "fname": "Pratush",
+        "lname": "Sharma",
+        "tel": "99803-49924",
+        "address": "Noida",
+        "city": "G B Nagar",
+        "state": "UP",
+        "zip": "201304"
+    },
+    {
+       "fname": "Nikhil",
+        "lname": "Sharma",
+        "tel": "99803-49924",
+        "address": "Noida",
+        "city": "G B Nagar",
+        "state": "UP",
+        "zip": "201304"
+    },
+    {
+        "fname": "Prashant",
+        "lname": "Sharma",
+        "tel": "99803-49924",
+        "address": "Noida",
+        "city": "G B Nagar",
+        "state": "UP",
+        "zip": "201304"
+    },
+    {
+        "fname": "Ola",
+        "lname": "Sharma",
+        "tel": "99803-49924",
+        "address": "Noida",
+        "city": "G B Nagar",
+        "state": "UP",
+        "zip": "201304"
+    },
+    {
+        "fname": "Deco",
+        "lname": "Sharma",
+        "tel": "99803-49924",
+        "address": "Noida",
+        "city": "G B Nagar",
+        "state": "UP",
+        "zip": "201304"
+    },
+    {
+        "fname": "Lokesh",
+        "lname": "Sharma",
+        "tel": "99803-49924",
+        "address": "Noida",
+        "city": "G B Nagar",
+        "state": "UP",
+        "zip": "201304"
+    }
+]
