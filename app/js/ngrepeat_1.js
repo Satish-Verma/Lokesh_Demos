33bad68951e122faf309b09adba5ad3385ef5e30
Satish-Verma <sattyto@gmail.com>
2017-01-23 12:52:16 +0530

    Create ngrepeat_1.js

---
+++ app/js/ngrepeat_1.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var app = angular.module('myApp', []);
+app.controller('myController', myController);
+
+function myController($scope) {
+  $scope.friends = [
+    {name:'John', age:25, gender:'boy'},
+    {name:'Jessie', age:30, gender:'girl'},
+    {name:'Johanna', age:28, gender:'girl'},
+    {name:'Joy', age:15, gender:'girl'},
+    {name:'Mary', age:28, gender:'girl'},
+    {name:'Peter', age:95, gender:'boy'},
+    {name:'Sebastian', age:50, gender:'boy'},
+    {name:'Erika', age:27, gender:'girl'},
+    {name:'Patrick', age:40, gender:'boy'},
+    {name:'Samantha', age:60, gender:'girl'}
+  ];
+}
+
